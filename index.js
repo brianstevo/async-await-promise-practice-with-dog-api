e4f31badc300b10f5e17c5145a8855bfa8020b34
brianstevo <brianaranha76@gmail.com>
2020-06-05 13:22:04 +0530

    resolved multiple callbacks into Promises

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-	console.log(`breed: ${data}`);
-	superagent
-		.get(`https://dog.ceo/api/breed/${data}/images/random`)
-		.end((err, res) => {
-			if (err) return console.log(err.message);
-
-			fs.writeFile('dog-img.txt', res.body.message, (err) => {
-				if (err) console.log(err.message);
-				console.log('saved');
-			});
+/*building promise*/
+const readFilePro = (file) => {
+	return new Promise((resolve, reject) => {
+		fs.readFile(file, (err, data) => {
+			if (err) reject('file not found');
+			resolve(data);
 		});
-});
+	});
+};
+const writeFilePro = (file, data) => {
+	return new Promise((resolve, reject) => {
+		fs.writeFile(file, data, (err) => {
+			if (err) reject(err.message);
+			resolve('saved');
+		});
+	});
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+	.then((data) => {
+		console.log(`breed: ${data}`);
+		return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+	})
+	.then((res) => {
+		console.log(res.body.message);
+
+		return writeFilePro('dog-img.txt', res.body.message);
+	})
+	.then(() => {
+		console.log('saved');
+	})
+	.catch((err) => {
+		console.log(err.message);
+	});
