032467d71c58653a1e4d9ab92bb4e521be5f34fa
brianstevo <brianaranha76@gmail.com>
2020-06-06 15:08:18 +0530

    handling multiple promises simultaneously

--- index.js
+++ index.js
@@ -3,37 +3,47 @@ const superagent = require('superagent');
 
 /*building promise*/
 const readFilePro = (file) => {
-	return new Promise((resolve, reject) => {
-		fs.readFile(file, (err, data) => {
-			if (err) reject('file not found');
-			resolve(data);
-		});
-	});
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, (err, data) => {
+            if (err) reject('file not found');
+            resolve(data);
+        });
+    });
 };
 const writeFilePro = (file, data) => {
-	return new Promise((resolve, reject) => {
-		fs.writeFile(file, data, (err) => {
-			if (err) reject(err.message);
-			resolve('saved');
-		});
-	});
+    return new Promise((resolve, reject) => {
+        fs.writeFile(file, data, (err) => {
+            if (err) reject(err.message);
+            resolve('saved');
+        });
+    });
 };
 
 const getDogPic = async () => {
-	try {
-		const data = await readFilePro(`${__dirname}/dog.txt`);
-		console.log(`breed: ${data}`);
-		const res = await superagent.get(
-			`https://dog.ceo/api/breed/${data}/images/random`
-		);
+    try {
+        const data = await readFilePro(`${__dirname}/dog.txt`);
+        console.log(`breed: ${data}`);
+        // const res = await superagent.get(
+        // 	`https://dog.ceo/api/breed/${data}/images/random`
+        // );
+        const resPro1 = superagent.get(
+            `https://dog.ceo/api/breed/${data}/images/random`
+        );
+        const resPro2 = superagent.get(
+            `https://dog.ceo/api/breed/${data}/images/random`
+        );
+        const resPro3 = superagent.get(
+            `https://dog.ceo/api/breed/${data}/images/random`
+        );
+        const all = await Promise.all([resPro1, resPro2, resPro3]);
+        const imgs = all.map(el => el.body.message);
+        console.log(imgs);
 
-		console.log(res.body.message);
-
-		await writeFilePro('dog-img.txt', res.body.message);
-		console.log('saved');
-	} catch (err) {
-		console.log(err);
-	}
+        await writeFilePro('dog-img.txt', imgs.join('\n'));
+        console.log('saved');
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 getDogPic();
